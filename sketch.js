83aa5fb4e62b984eb24c672d61166727983940a0
Jasmin151107 <79517547+Jasmin151107@users.noreply.github.com>
2021-05-25 15:17:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,15 +9,17 @@ function setup(){
   createCanvas(400,400);
   database = firebase.database();
   
+  game = new Game()
+  game.getState()
+  game.start() 
+  
 }
 
 function draw(){
   background("white");
   
-  game = new Game()
-  game.getState()
-  game.start() 
+  
 
-    drawSprites();
+  
   
 }
